388ed2fd3d087fe908e5a28473a143f107bf6e52
piyush80012 <ps81263855@gmail.com>
2024-05-01 08:57:44 +0530

    colors name

---
+++ src/PrintColors.java
@@ -0,0 +1,11 @@
+public class PrintColors {
+    public static void main(String[] args) {
+        String red = "red";
+        String green = "green";
+        String blue = "blue";
+        System.out.println(red);
+        System.out.println(green);
+        System.out.println(blue);
+
+    }
+}
